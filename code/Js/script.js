d5c722dce6726ea1704cd8e700e80f657931a790
Mitchel <mitchvdlind@gmail.com>
2026-09-17 17:35:12 +0100

    text hiding/show

--- code/Js/script.js
+++ code/Js/script.js
@@ -1,14 +1,33 @@
+// Draai de afbeelding wanneer erop wordt geklikt
 const afbeelding = document.querySelector(".draai-afbeelding");
 
-afbeelding.addEventListener("click", function () {
-  afbeelding.animate(
-    [
-      { transform: "rotate(0deg)" },
-      { transform: "rotate(360deg)" }
-    ],
-    {
-      duration: 1000,
-      iterations: 1
-    }
-  );
-});
+if (afbeelding) {
+  afbeelding.addEventListener("click", function () {
+    afbeelding.animate(
+      [
+        { transform: "rotate(0deg)" },
+        { transform: "rotate(360deg)" }
+      ],
+      {
+        duration: 1000,
+        iterations: 1
+      }
+    );
+  });
+}
+
+
+// Toon of verberg de gebaklijst
+const menuKnop = document.querySelector(".menu-knop");
+const gebakLijst = document.querySelector("#gebak-lijst");
+
+if (menuKnop && gebakLijst) {
+  menuKnop.addEventListener("click", function () {
+    gebakLijst.hidden = !gebakLijst.hidden;
+
+    menuKnop.setAttribute(
+      "aria-expanded",
+      String(!gebakLijst.hidden)
+    );
+  });
+}
