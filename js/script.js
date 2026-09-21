e1b9845e2207d171c544235fd50fbdc527ff1318
M477ie <matthieu_96@hotmail.com>
2026-09-22 01:02:36 +0200

    rekentool toevoegen

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log("JavaScript werkt");
+
+function berekenInhoud() {
+    const hoogte = document.getElementById("hoogte").value;
+    const breedte = document.getElementById("breedte").value;
+    const diepte = document.getElementById("diepte").value;
+
+    const resultaat = hoogte * breedte * diepte;
+    document.getElementById("resultaat").innerHTML = resultaat + " cm³";
+}
